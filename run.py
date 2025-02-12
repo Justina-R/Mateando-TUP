c8b69998f625190ec1b52b5b5528f18abd0bab11
Justina-R <justina.rey2602@gmail.com>
2025-02-12 16:02:14 -0300

    run.py update

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from app import crear_app
+import os
 
 app = crear_app()
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Obtén el puerto desde la variable de entorno o usa el puerto 5000 por defecto
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
